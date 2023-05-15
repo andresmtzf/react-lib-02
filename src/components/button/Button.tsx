0e154d2fda7ea8ce5fc7c4e91438f8cd8238026e
Andres Martinez <andres.martinez@habil.mx>
2023-05-15 14:17:08 -0600

    versión 0.0.3 que permite exportar estilos en css por cada module

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,27 @@
+import React, { ButtonHTMLAttributes } from 'react'
+import './Button.css'
+
+export interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  primary?: boolean
+  backgroundColor?: string | null
+  size?: string
+  label: string
+}
+
+const Button: React.FC<Props> = ({
+  primary = false,
+  backgroundColor = null,
+  size = 'medium',
+  label,
+  ...props
+}) => {
+  const mode = primary ? 'button--primary' : 'button--secondary'
+
+  return (
+    <button type='button' className={`button button--${size} ${mode}`} {...props}>
+      {label}
+    </button>
+  )
+}
+
+export default Button
